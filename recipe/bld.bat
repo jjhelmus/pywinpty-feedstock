e43ba433a62725effa1573a7a32eeaa680fdfe52
Jonathan Helmus <jjhelmus@gmail.com>
2018-03-15 16:48:09 -0500

    pass -D macro

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
 if "%ARCH%" == "32" (set PLATFORM=x86) else (set PLATFORM=x64)
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+if "%ARCH%" == "32" (set MACRO=MS_WIN32) else (set PLATFORM=MS_WIN64)
+"%PYTHON%" setup.py install -D "%MACRO"--single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
