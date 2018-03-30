4be021cb61ad112bf78d2e7e3a9c2c6209cec056
Jonathan Helmus <jjhelmus@gmail.com>
2018-03-30 13:13:57 -0500

    Revert "pass -D macro"
    This reverts commit e43ba433a62725effa1573a7a32eeaa680fdfe52.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,3 @@
 if "%ARCH%" == "32" (set PLATFORM=x86) else (set PLATFORM=x64)
-if "%ARCH%" == "32" (set MACRO=MS_WIN32) else (set PLATFORM=MS_WIN64)
-"%PYTHON%" setup.py install -D "%MACRO"--single-version-externally-managed --record record.txt
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
