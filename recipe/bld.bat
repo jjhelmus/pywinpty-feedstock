feb22fffeb6bc001f17e7368e7843f4477643a16
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-11 18:45:38 -0300

    try pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 if "%ARCH%" == "32" (set PLATFORM=x86) else (set PLATFORM=x64)
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+"%PYTHON%" -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 if errorlevel 1 exit 1
